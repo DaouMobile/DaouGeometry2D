ed2cce570755e70a7b140aa83df366983308873f
vongvoro <vongvoro@daou.co.kr>
2024-04-29 08:07:32 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DaouGeometry2D",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DaouGeometry2D",
+            targets: ["DaouGeometry2D"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DaouGeometry2D"),
+        .testTarget(
+            name: "DaouGeometry2DTests",
+            dependencies: ["DaouGeometry2D"]),
+    ]
+)
