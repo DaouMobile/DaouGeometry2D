a9e899a8b8fc209551f57a2a706177e4bc5287b8
vongvoro <vongvoro@daou.co.kr>
2024-04-29 10:31:32 +0900

    Chore: DaouAngle 디펜던시 exact v.1.0.0으로 가져오도록 변경

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/DaouMobile/DaouAngle",
-                 branch: "main")
+                 exact: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
